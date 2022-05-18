237d1fb030717789365f265774e66801d30108b2
EugeneTsalko <tefworkmail@gmail.com>
2022-05-19 01:16:44 +0300

    feat: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathFiles = path.join(__dirname, 'files');
+const pathFilesCopy = path.join(__dirname, 'files-copy');
+
+//проверяем есть ли папка files-copy, if - если нет, else - если есть
+fs.access(pathFilesCopy, (err) => {
+  if (err) {
+    copyDir();
+  }
+  else fs.rm(pathFilesCopy, { recursive: true }, (err) => {
+    if (err) throw err;
+    copyDir();
+  });
+});
+
+function copyDir() {
+
+  function callback(err) {
+    if (err) throw err;
+  }
+
+  fs.mkdir(pathFilesCopy, { recursive: true }, callback);
+
+  fs.readdir(pathFiles, {withFileTypes: true}, (err, items) => {
+    if (err) throw err;
+
+    for (let i = 0; i < items.length; i++) {//сюда можно добавить не только по файлам, но и по папкам
+      if (items[i].isFile()) {
+        fs.copyFile(path.join(pathFiles, items[i].name), path.join(pathFilesCopy, items[i].name), callback);
+      }
+    }
+  });
+}
